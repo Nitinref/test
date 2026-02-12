6ab3996307d7d4ad71c538458a440884d1dcf7a5
nitin yadav <nitinyadav484220@gmail.com>
2026-02-12 12:42:59 +0530

    erfe

--- packages/cli/src/tests/fixtures/button.jsx
+++ packages/cli/src/tests/fixtures/button.jsx
@@ -1,9 +1,10 @@
 export default function Button() {
   return (
-    <button>Click Me yrr</button>
+    <button>Click Me yrr 555</button>
   );
 }
 const test = "Hello World"
 const test4 = "My name is nitin"
 const test2 = "My name is nitin1"
 const finalCheck = "Now It Should Work";
+const finalChec4k = "Now It Should Work";
