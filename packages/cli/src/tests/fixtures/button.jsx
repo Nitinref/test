95b881cdb50604c19dceada09c64e9d2c626f49b
nitin yadav <nitinyadav484220@gmail.com>
2026-02-12 12:37:26 +0530

    asa

--- packages/cli/src/tests/fixtures/button.jsx
+++ packages/cli/src/tests/fixtures/button.jsx
@@ -8,3 +8,4 @@ const test4 = "My name is nitin"
 const test2 = "My name is nitin1"
 const finalCheck = "Now It Should Work";
 const slashTest = "SLASH_COMMAND_TEST_001";
+const slashTest1 = "SLASH_COMMAND_TEST_001";
